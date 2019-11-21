a341ddd9a948b1385f3fd46df7c044cb19bc0b3b
Justyna <jkloc96@gmail.com>
2019-11-21 09:50:08 +0100

    add test class

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -0,0 +1,16 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookKeeperTest {
+
+	@Test
+	public void requestOneInvoiceWithOnePosition() {
+		 
+		//assertThat(actual, matcher);
+	}
+
+}
+//izolacja od taxpolicy
