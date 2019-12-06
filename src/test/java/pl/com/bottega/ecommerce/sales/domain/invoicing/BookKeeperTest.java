2cd5ba3884b588c36d12d96805a32affdaad21b2
Paweł Siciński <233008@edu.p.lodz.pl>
2019-12-06 20:07:55 +0100

    isolation: removing redundant state tests; first behavior test

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -10,6 +10,8 @@ import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class BookKeeperTest {
@@ -35,60 +37,6 @@ public class BookKeeperTest {
         assertThat(invoice, notNullValue());
         assertThat(items, notNullValue());
         assertThat(items.size(), is(1));
-    }
-
-    @Test
-    public void stateTest_invoiceWithOneEntryShouldShouldMatchClientData() {
-        ProductData productData = Mockito.mock(ProductData.class);
-        ProductType productType = ProductType.STANDARD;
-        when(productData.getType()).thenAnswer(invocationOnMock -> productType);
-        Money money = new Money(0.0);
-        RequestItem requestItem = new RequestItem(productData, 0, money);
-        ClientData clientData = new ClientData(new Id("001"), "prod001");
-        InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
-        invoiceRequest.add(requestItem);
-        BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
-        TaxPolicy taxPolicy = Mockito.mock(TaxPolicy.class);
-        Tax tax = new Tax(money, "tax_desc");
-        when(taxPolicy.calculateTax(productType, money)).thenAnswer(invocationOnMock -> tax);
-
-        Invoice resultInvoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
-        ClientData resultClientData = resultInvoice.getClient();
-
-        assertThat(resultInvoice, notNullValue());
-        assertThat(resultClientData, notNullValue());
-        assertThat(resultClientData, is(equalTo(clientData)));
-    }
-
-    @Test
-    public void stateTest_EntryDataShouldMatchForInvoiceWithOneEntry() {
-        ProductData productData = Mockito.mock(ProductData.class);
-        ProductType productType = ProductType.STANDARD;
-        when(productData.getType()).thenAnswer(invocationOnMock -> productType);
-        Money money = new Money(0.0);
-        int quantity = 0;
-        RequestItem requestItem = new RequestItem(productData, quantity, money);
-        ClientData clientData = new ClientData(new Id("001"), "prod001");
-        InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
-        invoiceRequest.add(requestItem);
-        BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
-        TaxPolicy taxPolicy = Mockito.mock(TaxPolicy.class);
-        Tax tax = new Tax(money, "tax_desc");
-        when(taxPolicy.calculateTax(productType, money)).thenAnswer(invocationOnMock -> tax);
-
-        Invoice resultInvoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
-        ClientData resultClientData = resultInvoice.getClient();
-        List<InvoiceLine> resultItems = resultInvoice.getItems();
-
-        assertThat(resultInvoice, notNullValue());
-        assertThat(resultClientData, notNullValue());
-        assertThat(resultItems, notNullValue());
-        assertThat(resultClientData, is(equalTo(clientData)));
-        assertThat(resultItems.size(), is(equalTo(1)));
-        InvoiceLine invoiceLine = resultItems.get(0);
-        assertThat(invoiceLine.getProduct(), is(equalTo(productData)));
-        assertThat(invoiceLine.getQuantity(), is(equalTo(quantity)));
-        assertThat(invoiceLine.getTax(), is(equalTo(tax)));
     }
 
     @Test
@@ -157,6 +105,38 @@ public class BookKeeperTest {
         assertThat(invoiceLine2.getProduct(), is(equalTo(productData2)));
         assertThat(invoiceLine2.getQuantity(), is(equalTo(quantity2)));
         assertThat(invoiceLine2.getTax(), is(equalTo(tax2)));
+    }
+
+    @Test
+    public void behaviourTest_invoiceWithTwoEntriesShouldCalculateTaxTwice() {
+        ProductData productData1 = Mockito.mock(ProductData.class);
+        ProductData productData2 = Mockito.mock(ProductData.class);
+        ProductType productType1 = ProductType.STANDARD;
+        ProductType productType2 = ProductType.DRUG;
+        when(productData1.getType()).thenAnswer(invocationOnMock -> productType1);
+        when(productData2.getType()).thenAnswer(invocationOnMock -> productType2);
+        Money money = new Money(0.0);
+        int quantity1 = 0;
+        int quantity2 = 1;
+        RequestItem requestItem1 = new RequestItem(productData1, quantity1, money);
+        RequestItem requestItem2 = new RequestItem(productData2, quantity2, money);
+        ClientData clientData1 = new ClientData(new Id("001"), "prod001");
+        InvoiceRequest invoiceRequest = new InvoiceRequest(clientData1);
+        invoiceRequest.add(requestItem1);
+        invoiceRequest.add(requestItem2);
+        BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
+        TaxPolicy taxPolicy = Mockito.mock(TaxPolicy.class);
+        Tax tax1 = new Tax(money, "tax_desc_1");
+        Tax tax2 = new Tax(money, "tax_desc_2");
+        when(taxPolicy.calculateTax(productType1, money)).thenAnswer(invocationOnMock -> tax1);
+        when(taxPolicy.calculateTax(productType2, money)).thenAnswer(invocationOnMock -> tax2);
+
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+
+        assertThat(invoice, notNullValue());
+        verify(taxPolicy, times(1)).calculateTax(productType1, money);
+        verify(taxPolicy, times(1)).calculateTax(productType2, money);
+        verify(taxPolicy, times(2));
     }
 
 }
