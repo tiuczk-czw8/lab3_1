c7a228787c50e329d3e79ff3fe25d0e0041536e8
Mark0tny <marcinkotulski95@gmail.com>
2019-12-09 23:15:34 +0100

    isolation: add test for checking one entry one item in invoice

---
+++ src/test/java/BookKeeperTests.java
@@ -0,0 +1,51 @@
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.*;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.isNull;
+import static org.mockito.Mockito.when;
+
+public class BookKeeperTests {
+
+    private ProductData productData;
+    private ProductType productType;
+    private TaxPolicy taxPolicy;
+    private Money money;
+    private Tax tax;
+
+    @Before
+    public void initialize() {
+        productData = Mockito.mock(ProductData.class);
+        productType = ProductType.STANDARD;
+        taxPolicy = Mockito.mock(TaxPolicy.class);
+        money = new Money(5.0);
+        tax = new Tax(money, "taxes");
+    }
+
+    @Test
+    public void invoiceWithOneEntryForOneListElement() {
+        when(productData.getType()).thenAnswer(invocationOnMock -> productType);
+        RequestItem requestItem = new RequestItem(productData, 0, money);
+        ClientData clientData = new ClientData(new Id("1"), "prod");
+        InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+        invoiceRequest.add(requestItem);
+        BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
+        when(taxPolicy.calculateTax(productType, money)).thenAnswer(invocationOnMock -> tax);
+
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+        List<InvoiceLine> items = invoice.getItems();
+
+        assertThat(invoice, notNullValue());
+        assertThat(items, notNullValue());
+        assertThat(items.size(), is(1));
+    }
+}
