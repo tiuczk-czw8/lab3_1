b588e16ecc1d6b77292251d77645ddfaa83d2b84
Mark0tny <marcinkotulski95@gmail.com>
2019-12-09 23:18:51 +0100

    isolation: add test to verify calculate tax was invoked twice

--- src/test/java/BookKeeperTests.java
+++ src/test/java/BookKeeperTests.java
@@ -3,6 +3,7 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.mockito.verification.VerificationMode;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.invoicing.*;
@@ -12,7 +13,7 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.isNull;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class BookKeeperTests {
 
@@ -47,5 +48,24 @@ public class BookKeeperTests {
         assertThat(invoice, notNullValue());
         assertThat(items, notNullValue());
         assertThat(items.size(), is(1));
+    }
+
+    @Test
+    public void calculateTaxTwiceForInvoiceWithTwoEntries() {
+        when(productData.getType()).thenAnswer(invocationOnMock -> productType);
+        RequestItem requestItem = new RequestItem(productData, 0, money);
+        ClientData clientData = new ClientData(new Id("1"), "product");
+        InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+        invoiceRequest.add(requestItem);
+        invoiceRequest.add(requestItem);
+        BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
+        when(taxPolicy.calculateTax(productType, money)).thenAnswer(invocationOnMock -> tax);
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+        List<InvoiceLine> items = invoice.getItems();
+        verify(taxPolicy, times(2)).calculateTax(productType, money);
+
+        assertThat(invoice, notNullValue());
+        assertThat(items, notNullValue());
+        assertThat(items.size(), is(2));
     }
 }
