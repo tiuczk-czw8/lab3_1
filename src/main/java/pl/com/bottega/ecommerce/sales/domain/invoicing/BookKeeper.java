20dcc0f0200144d4c1e55b5aa9d99daa3fe287d7
Strzepek.Aleksander <233010@edu.p.lodz.pl>
2019-12-11 22:50:24 +0100

    isolation first test added

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -1,17 +1,14 @@
 /*
  * Copyright 2011-2014 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
@@ -19,24 +16,26 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeper {
 
-	private InvoiceFactory invoiceFactory;
-	public BookKeeper(InvoiceFactory invoiceFactory) {
-		this.invoiceFactory = invoiceFactory;
-	}
-	public Invoice issuance(InvoiceRequest invoiceRequest, TaxPolicy taxPolicy) {
-		Invoice invoice = invoiceFactory.create(invoiceRequest.getClientData());
+    private InvoiceFactory invoiceFactory;
 
-		for (RequestItem item : invoiceRequest.getItems()) {
-			Money net = item.getTotalCost();
-			Tax tax = taxPolicy.calculateTax(item.getProductData().getType(),
-					net);
+    public BookKeeper(InvoiceFactory invoiceFactory) {
+        this.invoiceFactory = invoiceFactory;
+    }
 
-			InvoiceLine invoiceLine = new InvoiceLine(item.getProductData(),
-					item.getQuantity(), net, tax);
-			invoice.addItem(invoiceLine);
-		}
+    public Invoice issuance(InvoiceRequest invoiceRequest, TaxPolicy taxPolicy) {
+        Invoice invoice = invoiceFactory.create(invoiceRequest.getClientData());
 
-		return invoice;
-	}
+        for (RequestItem item : invoiceRequest.getItems()) {
+            Money net = item.getTotalCost();
+            Tax tax = taxPolicy.calculateTax(item.getProductData()
+                                                 .getType(),
+                    net);
+
+            InvoiceLine invoiceLine = new InvoiceLine(item.getProductData(), item.getQuantity(), net, tax);
+            invoice.addItem(invoiceLine);
+        }
+
+        return invoice;
+    }
 
 }
--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,28 +1,54 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeperTest {
 
+    private ProductType productType = ProductType.STANDARD;
+    private ProductData productData = mock(ProductData.class);
+    private TaxPolicy taxPolicy = Mockito.mock(TaxPolicy.class);
+    private BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
+    private Money money = new Money(2.0f);
+    private Money net = new Money(1.0f);
+    private Tax tax = new Tax(money, "tax");
+    private ClientData clientData = new ClientData(new Id("1"), "name");
+    private InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+
     @Before
     public void setup() {
-
+        // when(this.productData.getType()).thenReturn(productType);
+        // when(taxPolicy.calculateTax(productType, net)).thenReturn(tax);
+        when(productData.getType()).thenAnswer(invocationOnMock -> productType);
+        when(taxPolicy.calculateTax(productType, money)).thenAnswer(invocationOnMock -> tax);
     }
 
     @Test
-    public void aaaaa() {
+    public void stateTestReturnInvoiceWithOneEntry() {
+        RequestItem requestItem = new RequestItem(productData, 1, money);
+        invoiceRequest.add(requestItem);
 
-        InvoiceFactory mockedInvoiceFactory = mock(InvoiceFactory.class);
-        ClientData mockedClientData = mock(ClientData.class);
-        // when(mockedInvoiceFactory.create(mockedClientData)).thenReturn("first");
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+        List<InvoiceLine> items = invoice.getItems();
 
-        BookKeeper bookKeeper = new BookKeeper(mockedInvoiceFactory);
-        // assertThat(tails, Matchers.hasSize(HELLO.length() + 1));
+        assertThat(invoice, notNullValue());
+        assertThat(items, notNullValue());
+        assertThat(items.size(), is(1));
     }
 
 }
