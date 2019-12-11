532a4da633522110d194743ed3e3d1fbad60a3c4
dawid.krawczyk <dawid.krawczyk@kernelpanic.com>
2019-12-11 19:59:28 +0100

    Invoke calculate tax two times for two entries

--- src/test/java/BookKeeperTest.java
+++ src/test/java/BookKeeperTest.java
@@ -12,13 +12,18 @@ import java.util.List;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class BookKeeperTest {
     private ProductData productData;
     private ProductType productType = ProductType.STANDARD;
     private TaxPolicy taxPolicy;
+    private Money money;
+    private RequestItem requestItem;
+    private ClientData clientData;
+    private InvoiceRequest invoiceRequest;
+    private BookKeeper bookKeeper;
+    private Tax tax;
 
     @Before
     public void mockClass() {
@@ -27,21 +32,36 @@ public class BookKeeperTest {
         when(productData.getType()).thenAnswer(invocationOnMock -> productType);
     }
 
+    private void initData() {
+        money = new Money(0.0);
+        requestItem = new RequestItem(productData, 0, money);
+        clientData = new ClientData(new Id("1"), "prod");
+        invoiceRequest = new InvoiceRequest(clientData);
+        bookKeeper = new BookKeeper(new InvoiceFactory());
+        tax = new Tax(money, "tax");
+    }
+
     @Test
     public void shouldReturnInvoiceWithOneEntryForOneListElement() {
-        Money money = new Money(0.0);
-        RequestItem requestItem = new RequestItem(productData, 0, money);
-        ClientData clientData = new ClientData(new Id("1"), "prod");
-        InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+        initData();
         invoiceRequest.add(requestItem);
-        BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
-        Tax tax = new Tax(money, "tax");
         when(taxPolicy.calculateTax(productType, money)).thenAnswer(invocationOnMock -> tax);
         Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
         List<InvoiceLine> itemList = invoice.getItems();
-
         assertThat(invoice, notNullValue());
         assertThat(itemList, notNullValue());
         assertThat(itemList.size(), is(1));
+    }
+
+    @Test
+    public void shouldInvokeCalculateTaxTwoTimesForTwoEntries() {
+        initData();
+        invoiceRequest.add(requestItem);
+        invoiceRequest.add(requestItem);
+        when(taxPolicy.calculateTax(productType, money)).thenAnswer(invocationOnMock -> tax);
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+        List<InvoiceLine> items = invoice.getItems();
+        assertThat(items.size(), is(2));
+        verify(taxPolicy, times(2)).calculateTax(productType, money);           //should invoke calculateTax two times
     }
 }
